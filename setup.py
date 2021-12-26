362dc6f255a32de16d96665dbae73c9cc35f8cd4
esakov-s <54104577+esakov-s@users.noreply.github.com>
2021-12-26 16:27:25 +0300

    Всё для библиотеки

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+install_requires=[math, random, matplotlib.pyplot, prettytable.PrettyTable]
+setup(name='Diff_Integr',
+      version='0.0.0',
+      description='Differentiation and Integration',
+      packages=['Diff_Integr'],
+      author_email ='slava.esakov@mail.ru',
+      zip_safe=False)
